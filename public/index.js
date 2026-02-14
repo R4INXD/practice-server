46f1156996da5604811edc7d0373297bb067e463
jinpill <hello@jinpill.dev>
2026-02-14 21:30:33 +0900

    로그인 API 핸들러 수정

--- public/index.js
+++ public/index.js
@@ -6,13 +6,11 @@ button.addEventListener("click", () => {
   request("/login", "POST", {
     id: id.value,
     password: password.value,
-  }).then((res) => {
-    if (res.ok) {
-      alert("로그인 성공!!");
-    } else if (res.status === 401) {
-      alert("없는 계정입니다!");
+  }).then((response) => {
+    if (response.code === "SUCCESS") {
+      alert(response.message);
     } else {
-      alert("예상치 못한 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+      alert(response.message);
     }
   });
 });
