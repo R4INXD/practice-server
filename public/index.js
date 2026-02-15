ce38ea265fa062497afc8959e752e8dba1aa70a9
jinpill <hello@jinpill.dev>
2026-02-16 00:47:51 +0900

    로그인 성공시 유저 정보 표시

--- public/index.js
+++ public/index.js
@@ -1,16 +1,36 @@
 const id = document.getElementById("id");
 const password = document.getElementById("password");
-const button = document.getElementById("login");
+const loginButton = document.getElementById("login");
+const logoutButton = document.getElementById("logout");
 
-button.addEventListener("click", () => {
+loginButton.addEventListener("click", () => {
   request("/login", "POST", {
     id: id.value,
     password: password.value,
   }).then((response) => {
-    if (response.code === "SUCCESS") {
+    if (response.code !== "SUCCESS") {
       alert(response.message);
-    } else {
-      alert(response.message);
+      return;
     }
+
+    const loginForm = document.getElementById("login-form");
+    const userInfo = document.getElementById("user-info");
+    loginForm.style.display = "none";
+    userInfo.style.display = "block";
+
+    const userName = document.getElementById("user-name");
+    const createdAt = document.getElementById("created-at");
+    userName.innerText = response.data.name;
+    createdAt.innerText = response.data.createdAt;
   });
+});
+
+logoutButton.addEventListener("click", () => {
+  const loginForm = document.getElementById("login-form");
+  const userInfo = document.getElementById("user-info");
+  loginForm.style.display = "block";
+  userInfo.style.display = "none";
+
+  id.value = "";
+  password.value = "";
 });
