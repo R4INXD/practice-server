3679669f622dc1d739f63cde95c3891441cc5842
jinpill <hello@jinpill.dev>
2026-02-15 22:46:39 +0900

    회원가입 페이지 개선

--- public/join.js
+++ public/join.js
@@ -1,15 +1,47 @@
 // public/join.js
 
 const id = document.getElementById("id");
-const password = document.getElementById("password");
+const pass = document.getElementById("password");
+const confirm = document.getElementById("password-confirm");
+const name = document.getElementById("name");
 const button = document.getElementById("join");
 
 button.addEventListener("click", () => {
+  if (!checkValues()) return;
+  if (pass.value !== confirm.value) {
+    alert("비밀번호가 일치하지 않습니다.");
+    return;
+  }
+
   request("/join", "POST", {
     id: id.value,
-    password: password.value,
+    password: pass.value,
+    name: name.value,
   }).then((response) => {
-    console.log(response);
     alert(response.message);
   });
 });
+
+const checkValues = () => {
+  if (!id.value) {
+    alert("아이디를 입력해주세요.");
+    return false;
+  }
+
+  if (!pass.value) {
+    alert("비밀번호를 입력해주세요.");
+    return false;
+  }
+
+  if (!confirm.value) {
+    alert("비밀번호 확인을 입력해주세요.");
+    return false;
+  }
+
+  if (!name.value) {
+    alert("이름을 입력해주세요.");
+    return false;
+  }
+
+  return true;
+};
